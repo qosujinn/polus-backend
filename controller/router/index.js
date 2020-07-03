862064c3767204001cb6828afd34a75885f730ba
taefyi <jermaine.fyi@gmail.com>
2020-07-03 15:33:31 -0500

    server config and boot up is a success

--- controller/router/index.js
+++ controller/router/index.js
@@ -1,9 +1,14 @@
 module.exports = ( expr ) => {
    //root
-   expr.get('/', ( req, res ) => {
-      res.send(`Hello there, ${ req.user.displayName }!`)
+   expr.get( '/', ( req, res ) => {
+      res.send(`Hello there!`)
    })
-   
+
+   //ping
+   expr.get( '/ping', ( req, res ) => {
+      res.status(200).end('PINGING IT')
+   })
+
    //if there ever is an error
    expr.use( ( err, req, res, next ) => {
       console.error( err.stack || err.message )
--- main.js
+++ main.js
@@ -1,61 +1,20 @@
-const expr = require('express')(),
-CONF = require('./lib/.config')
+const expr = require('express')()
+const { Shibboleth } = require('shibboleth'),
+CONF = require('./lib/.config'),
+shibb = new Shibboleth( CONF.shibboleth.url )
 
 let http = require('http'), https = require('https'),
 fs = require('fs'),
 options = {
-	key: fs.readFileSync('./lib/security/key.pem'),
-	cert: fs.readFileSync('./lib/security/cert.pem')
+	key: fs.readFileSync(`./lib/security/${CONF.env.key_name}`),
+	cert: fs.readFileSync(`./lib/security/${CONF.env.cert_name}`)
 }
 
-//express middleware
-let passport = require('passport'),
-morg = require('morgan'),
-sess = require('express-session'),
-shibb = require('passport-uwshib')
+
 
 expr.use( require('body-parser').json() ) 
-.use( require('body-parser').urlencoded({ extended: true }) )
-.use( require('cors')() ) 
-.use( passport.initialize() )
-.use( passport.session() )
-.use( require('cookie-parser') )
-.use( morg(CONF.logformat || 'dev') )
-.use( sess({ 
-	secret: fs.readFileSync( './lib/security/sess-secret.txt', 'utf-8' ),
-	cookie: { secret: true },
-	resave: false,
-	saveUninitialized: false 
-}) )
-
-//create the Shibboleth strategy
-let shibbstrat = new shibb.Strategy( {
-	entityId: CONF.domain,
-	privateKey: options.key,
-	callbackUrl: '/auth/callback',
-	domain: CONF.domain,
-	acceptedClockSkewMs: 200
-})
-//tell Passport to use it
-passport.use( shibbstrat )
-
-//serialize and deserialize calls for user sessions
-passport.serializeUser( ( user, done ) => {
-	done( null, user )
-})
-
-passport.deserializeUser( ( user, done ) => {
-	done( null, user )
-})
-
-expr.get('/auth', passport.authenticate( shibbstrat.name ), shibb.backToUrl() )
-expr.post('/auth/callback', passport.authenticate( shibbstrat.name ), shibb.backToUrl() )
-expr.get( shibb.urls.metadata, shibb.metadataRoute( shibbstrat, options.cert ) )
-
-expr.use( shibb.ensureAuth('/auth') )
-
-//pass the express object to the router
-require('./controller/router')( expr )
+expr.use( require('body-parser').urlencoded({ extended: true }) )
+expr.use( require('cors')() ) 
 
 //start the https server, passing the options and the express object
 https.createServer( options, expr ).listen( CONF.env.port_https, () => {
@@ -63,11 +22,15 @@ https.createServer( options, expr ).listen( CONF.env.port_https, () => {
 })
 
 http.createServer( ( req, res ) => {
-	let redirect = `https://${CONF.domain}`
+	let redirect = `https://${CONF.env.domain}`
 
 	if( CONF.env.port_https != 443 )
 		redirect += `:${CONF.env.port_https}`
 
 	redirect += req.url
 	res.writeHead( 301, { 'Location': redirect }).end()
-}).listen( CONF.env.port_http )
+}).listen( CONF.env.port_http, () => {
+	console.log(`http server's running on port ${ CONF.env.port_http }`)
+})
+
+require('./controller/router')( expr )
